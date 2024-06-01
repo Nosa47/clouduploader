be1d5bc95470f8d0b494bc4e5a9453f4c781d188
Nosa47 <wadidessy@gmail.com>
2024-06-01 00:12:48 +0000

    finished work and final commit

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 # Path to install the script
 INSTALL_DIR="/usr/local/bin"
 
-# Copy the script file to the installation directory
-cp clouduploader.sh "$INSTALL_DIR"
+# Copy the renamed script file to the installation directory
+cp clouduploader "$INSTALL_DIR"
 
 # Make the script executable
-chmod +x "$INSTALL_DIR/clouduploader.sh"
+chmod +x "$INSTALL_DIR/clouduploader"
 
 echo "Cloud Uploader CLI Tool installed successfully!"
