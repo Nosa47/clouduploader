b4a492679db6168a31cacf16eca34b29b55c65cb
Nosa47 <wadidessy@gmail.com>
2024-06-03 13:23:41 +0000

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# Path to install the script
+# Path to the script file
+SCRIPT_FILE="clouduploader.sh"
+
+# Destination directory for installation
 INSTALL_DIR="/usr/local/bin"
 
-# Move the renamed script file to the installation directory
-mv clouduploader "$INSTALL_DIR"
+# Check if the script file exists
+if [ ! -f "$SCRIPT_FILE" ]; then
+    echo "Error: '$SCRIPT_FILE' not found."
+    exit 1
+fi
+
+# Move the script file to the installation directory
+mv "$SCRIPT_FILE" "$INSTALL_DIR/clouduploader"
 
 # Make the script executable
 chmod +x "$INSTALL_DIR/clouduploader"
